65bf07edc016b493a3fc0d64f6cc35688c3b202e
Wu, Zhenyu <wuzhenyu@ustc.edu>
2026-02-13 21:26:19 +0800

    :adhesive_bandage: Support lazy loading

---
+++ lua/tellenc/nvim.lua
@@ -0,0 +1,45 @@
+---lazy load
+-- luacheck: ignore 112 113
+---@diagnostic disable: undefined-global
+local M = {}
+
+---don't use
+---table.concat(vim.api.nvim_buf_get_text(0, 0, 0, -1, -1, {}))
+---because vim has encoding
+function M.callback()
+    if vim.o.buftype ~= "" then
+        return
+    end
+    local fd = io.open(vim.api.nvim_buf_get_name(0))
+    if fd == nil then
+        return
+    end
+    local text = fd:read()
+    fd:close()
+    if text == nil then
+        return
+    end
+    local tellenc = require 'tellenc.tellenc'.tellenc
+    local enc = tellenc(text)
+    if enc == "" or enc == vim.o.fileencoding then
+        return
+    end
+    -- cannot work
+    -- vim.bo.fileencoding = enc
+    -- vim.o.fileencodings = enc
+    -- https://github.com/neovim/neovim/issues/21687
+    vim.cmd("edit ++encoding=" .. enc)
+end
+
+---create autocmds
+---@param augroup_id integer?
+function M.create_autocmds(augroup_id)
+    augroup_id = augroup_id or vim.api.nvim_create_augroup("tellenc", {})
+    -- BufAdd/BufNew cannot work
+    vim.api.nvim_create_autocmd("BufEnter", {
+        group = augroup_id,
+        callback = M.callback
+    })
+end
+
+return M
---
+++ lua/tellenc/tellenc.lua
@@ -0,0 +1,23 @@
+---tellenc for neovim
+local tellenc = require "tellenc".tellenc
+local M = {
+    encs = {
+        ascii = "utf-8",
+        gbk = "cp936",
+        gb2312 = "cp936",
+        big5 = "cp950",
+        binary = "",
+        unknown = "",
+    },
+}
+
+---wrap `tellenc`
+---@param text string
+---@return string enc
+function M.tellenc(text)
+    local enc = tellenc(text)
+    enc = M.encs[enc] or enc
+    return enc
+end
+
+return M
